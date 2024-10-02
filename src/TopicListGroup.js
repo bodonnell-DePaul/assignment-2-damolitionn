16195e075e58de59b46e659db987c89f41668266
damolitionn <87623949+damolitionn@users.noreply.github.com>
2024-10-02 22:05:47 +0000

    Made fields editable

--- src/TopicListGroup.js
+++ src/TopicListGroup.js
@@ -1,32 +1,49 @@
-import {ListGroup, Col, Row, Tab} from 'react-bootstrap';
+import { ListGroup, Col, Row, Tab } from 'react-bootstrap';
+import { useState } from 'react';
 import todos from './todoItems';
 
 function TopicListGroup() {
-  return (
-    <Tab.Container id="ListGroupWithTabs" defaultActiveKey="0">
-      <Row>
-        <Col sm={3}>
-          <ListGroup>
-            {todos.map((todo, index) => (
-              <ListGroup.Item action href={`#${index}`} key={index} variant = {GetListColour(todo.dueDate)}>
-                {todo.title}
-              </ListGroup.Item>
-            ))}
-          </ListGroup>
-        </Col>
-        <Col sm={9}>
-          <Tab.Content>
-            {todos.map((todo, index) => (
-              <Tab.Pane key={index} eventKey={`#${index}`}>
-                <p>{todo.description}</p>
-                <p>{todo.dueDate}</p>
-              </Tab.Pane>
-            ))}
-          </Tab.Content>
-        </Col>
-      </Row>
-    </Tab.Container>
-  );
+    const [todoItems, setTodoItems] = useState(todos);
+
+    const ChangedDate = (index, newDate) => {
+        const updatedTodos = [...todoItems];
+        updatedTodos[index].dueDate = newDate;
+        setTodoItems(updatedTodos);
+    };
+
+    const ChangedDescription = (index, newDescription) => {
+        const updatedTodos = [...todoItems];
+        updatedTodos[index].description = newDescription;
+        setTodoItems(updatedTodos);
+    };
+
+    return (
+        <Tab.Container id="ListGroupWithTabs" defaultActiveKey="0">
+        <Row>
+            <Col sm={3}>
+            <ListGroup>
+                {todos.map((todo, index) => (
+                <ListGroup.Item action href={`#${index}`} key={index} variant = {GetListColour(todo.dueDate)}>
+                    {todo.title}
+                </ListGroup.Item>
+                ))}
+            </ListGroup>
+            </Col>
+            <Col sm={9}>
+            <Tab.Content>
+                {todos.map((todo, index) => (
+                <Tab.Pane key={index} eventKey={`#${index}`}>
+                    <div contentEditable suppressContentEditableWarning onBlur={(e) => ChangedDescription(index, e.target.innerText)}>
+                        {todo.description}
+                    </div>
+                    <input type="date" value={todo.dueDate} onChange={(e) => ChangedDate(index, e.target.value)}/>
+                </Tab.Pane>
+                ))}
+            </Tab.Content>
+            </Col>
+        </Row>
+        </Tab.Container>
+    );
 }
 
 function GetListColour(dueDate) {
